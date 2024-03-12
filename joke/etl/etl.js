a1eee946db114fd4e11e5d78c7588e24567e68ac
lehan but goated <lehande28@gmail.com>
2024-03-12 11:05:04 +0000

    first commit

---
+++ joke/etl/etl.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const amqp = require("amqplib");
+
+const APP_CONSUMER1_PORT = 3001;
+const RMQ_CONSUMER1_PORT = 5672;
+const QUEUE_NAME = process.env.QUEUE_NAME || "joke";
+const RMQ_USER_NAME = "admin";
+const RMQ_PASSWORD = "admin";
+//const RMQ_HOST = '10.1.0.8'  // Private IP of the vm hosting rabbitmq
+const RMQ_HOST = "localhost"; // If accessing local mq container
+//const RMQ_HOST = 'host.docker.internal'
+//const RMQ_HOST = '20.108.32.75'
+
+app.get("/", (req, res) => {
+  res.send(`ETL is up`);
+});
+
+const server = app.listen(APP_CONSUMER1_PORT, () =>
+  console.log(`Listening on port ${APP_CONSUMER1_PORT}`)
+);
+
+async function getMessages(channel, queue) {
+  try {
+    await channel.assertQueue(queue, { durable: true }); // Connect to durable queue or create if not there
+
+    // Create callback that will listen for queued message availability
+    channel.consume(queue, (message) => {
+      let msg = JSON.parse(message.content.toString()); // Convert message to string then json -> msg
+      console.log(msg); // Just output or, say write to a file, database or whatever
+      channel.ack(message); // Ack message so it will be removed from the queue
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
+// Create connection and channel and return them to the caller
+async function createConnection(conStr) {
+  try {
+    const connection = await amqp.connect(conStr); // Create connection
+    console.log(`Connected to Rabbitmq cluster`);
+
+    const channel = await connection.createChannel(); // Create channel. Channel can have multiple queues
+    console.log(`Channel created. Will connect to queue: ${QUEUE_NAME}`);
+
+    return { connection, channel };
+  } catch (err) {
+    console.log(`Failed to connect to RabbitMQ`);
+    throw err;
+  }
+}
+
+// This is a very simple consumer for the tv queue. Run it once and it will consume any messages in the queue
+// You need to acknowledge receipt for it to be deleted
+// Demo shows how you can look for specific queue and even specific messages - other apps may be looking for others
+(async () => {
+  // const conStr = `amqp://${RMQ_USER_NAME}:${RMQ_PASSWORD}@${RMQ_HOST}:${RMQ_CONSUMER1_PORT}/`
+  // Alternatively, create connection with an object to provide settings other than default
+
+  const conStr = {
+    hostname: RMQ_HOST,
+    port: RMQ_CONSUMER1_PORT,
+    username: RMQ_USER_NAME,
+    password: RMQ_PASSWORD,
+    vhost: "/",
+    reconnect: true, // Enable automatic reconnection
+    reconnectBackoffStrategy: "linear", // or 'exponential'
+  };
+
+  try {
+    const rmq = await createConnection(conStr); // amqplib is promise based so need to initialise it in a function as await only works in an async function
+    console.log(`Connection created using: ${conStr}`);
+    connection = rmq.connection; // Available if needed for something
+    channel = rmq.channel;
+    console.log(`Channel opened on Consumer1`);
+    getMessages(channel, QUEUE_NAME); // Call to start the consumer callback
+  } catch (err) {
+    console.log(`General error: ${err}`);
+    throw err;
+  }
+})().catch((err) => {
+  console.log(
+    `Shutting down node server listening on port ${APP_CONSUMER1_PORT}`
+  );
+  server.close(); // Close the http server created with app.listen
+  console.log(`Closing app with process.exit(1)`);
+  process.exit(1); // Exit process with an error to force the container to stop
+});
---
+++ submit/submit.js
@@ -0,0 +1,197 @@
+// RabbitMQ demo. Does nothing fancy - uses defaults. Queue is not set to be durable after container restart
+const express = require("express");
+const app = express();
+const amqp = require("amqplib"); // Documentation here: https://www.npmjs.com/package/amqp
+app.use(express.json()); // Enable json POST
+
+const APP_PRODUCER_PORT = 4200;
+const RMQ_PRODUCER_PORT = 4201;
+const RMQ_USER_NAME = "admin";
+const RMQ_PASSWORD = "admin";
+//const RMQ_HOST = '20.90.112.187' // If mq is running in cloud but attaching from local vscode
+//const RMQ_HOST = 'rabbitmq'    // Docker DNS for mq if conecting from a container whether local or not
+const RMQ_HOST = "localhost"; // Connect to local container from vscode
+//const RMQ_HOST = host.docker.internal
+let gConnection; // File scope so functions can use them
+let gChannel;
+
+app.get("/", (req, res) => {
+  res.send(`Producer is up`);
+});
+
+// Generate some random messages for the queue. Call with:
+// localhost:3000/rand to generate one tv type
+// localhost:3000/rand?cat=computer&num=5 to generate 5 computer type messages
+app.get("/rand", async (req, res) => {
+  let category = req.query.cat ? req.query.cat : "tv";
+  let numMsgs = req.query.num ? req.query.num : 1;
+
+  try {
+    await sendRandMsg(gChannel, category, numMsgs);
+    res.status(202).send(`${numMsgs} queued`); // 202 = accepted. i.e. added to a queue but not necessarily processed yet
+  } catch (err) {
+    res.status(500).send(err); // Server error
+  }
+});
+
+// This will output a random mixture of the topics
+// localhost:3000/mix?num=5 to generate 5 mixed computer and tv type messages
+app.get("/mix", async (req, res) => {
+  let numMsgs = req.query.num ? req.query.num : 1;
+  for (i = 0; i < numMsgs; i++) {
+    try {
+      await sendRandMsg(gChannel, "mix", numMsgs);
+    } catch (err) {
+      res.status(500).send(err); // Server error
+    }
+  }
+  res.status(202).send(`${numMsgs} queued`); // 202 = accepted. i.e. added to a queue but not necessarily processed yet
+});
+
+// Post a single message to the appropriate queue based on
+app.post("/msg", async (req, res) => {
+  try {
+    await sendMsg(gChannel, req.body);
+    res.sendStatus(202);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// app.listen returns an http server. Use this if we need to access the server - e.g. stop it
+const server = app.listen(
+  APP_PRODUCER_PORT,
+  console.log(`Listening on port ${APP_PRODUCER_PORT}`)
+);
+
+/********************** Functions *************************************/
+
+async function createConnection(conStr) {
+  try {
+    const connection = await amqp.connect(conStr); // Create connection
+    console.log(`Connected to rabbitmq using ${conStr}`);
+
+    const channel = await connection.createChannel(); // Create channel. Channel can have multiple queues
+    console.log(`Channel created`);
+
+    return { connection, channel };
+  } catch (err) {
+    console.log(`Failed to connect to queue in createConection function`);
+    throw err;
+  }
+}
+
+// If needed, this is a function to close the queue connections
+async function closeConnection(connection, channel) {
+  try {
+    await channel.close();
+    await connection.close();
+    console.log(`Connection and channel closed`);
+  } catch (err) {
+    console.log(`Failed to close connection. ${err}`);
+  }
+}
+
+// Queue one or more messages on the category queue
+// Each message is randomly geerated by the createMessage function then queued
+async function sendRandMsg(channel, categ, numMsgs) {
+  const cat = ["tv", "computer"];
+  for (i = 0; i < numMsgs; i++) {
+    if (categ == "mix") {
+      category = cat[getRand(0, cat.length - 1)];
+    } else {
+      category = categ;
+    }
+
+    try {
+      const res = await channel.assertQueue(category, { durable: true }); // Create queue called whatever is in category if one doesn't exist
+      console.log(`${category} queue created`);
+
+      let msg = createMessage(category);
+      await channel.sendToQueue(category, Buffer.from(JSON.stringify(msg)));
+      console.log(msg);
+    } catch (err) {
+      console.log(`Failed to write to ${category} queue. ${err}`);
+      throw err;
+    }
+  }
+}
+
+// This function writes one json message to the queue based on the msg.category property
+async function sendMsg(channel, msg) {
+  try {
+    const res = await channel.assertQueue(msg.category, { durable: true }); // Create queue called whatever is in category if one doesn't exist
+    console.log(`${msg.category} queue created / accessed`);
+    await channel.sendToQueue(msg.category, Buffer.from(JSON.stringify(msg)), {
+      persistent: true,
+    }); // Saves to volume to survive broker restart
+    console.log(msg);
+  } catch (err) {
+    console.log(`Failed to write to ${category} queue.${err}`);
+  }
+}
+
+// Create a realistic looking message of the specified type based on random selections from arrays of text
+// Only currently supports two types: computer and tv
+function createMessage(type) {
+  const compManufacturer = ["Dell", "HP", "Acer", "Asus", "Samsung", "Toshiba"];
+  const compDevice = ["Laptop", "desktop", "Monitor", "Keyboard"];
+  const tvManufacturer = ["Samsung", "Hitachi", "Toshiba", "Philips", "LG"];
+  const tvType = ["CRT", "LCD", "LED", "QLED"];
+
+  const msg = {};
+  switch (type) {
+    case "computer":
+      msg.make = compManufacturer[getRand(0, compManufacturer.length - 1)];
+      msg.device = compDevice[getRand(0, compDevice.length - 1)];
+      msg.cost = getRand(10, 3000);
+      break;
+
+    case "tv":
+      msg.make = tvManufacturer[getRand(0, tvManufacturer.length - 1)];
+      msg.type = tvType[getRand(0, tvType.length - 1)];
+      msg.cost = getRand(250, 5000);
+      break;
+  }
+
+  msg.cat = type;
+  return msg;
+}
+
+// Get a random number between lower and upper inclusive
+function getRand(lower, upper) {
+  lower = Math.ceil(lower);
+  upper = Math.floor(upper);
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+// To use await on createConnection, it needs to be called from within an async function
+// Created an Immediately-Invoked Function Expression (IIFE) function to do this
+// This is a function that is immediately invoked after declaration
+// syntax is:
+// ( function() {
+// })()
+(async () => {
+  const conStr = `amqp://${RMQ_USER_NAME}:${RMQ_PASSWORD}@${RMQ_HOST}:${RMQ_PRODUCER_PORT}/`;
+  try {
+    console.log(
+      `Trying to connect to RabbitMQ at ${RMQ_HOST}:${RMQ_PRODUCER_PORT}`
+    ); // Only give this level of detail away in testing
+    const rmq = await createConnection(conStr); // amqplib is promise based so need to initialise it in a function as await only works in an async function
+    gConnection = rmq.connection; // Globally available in the file for other functions to use if needed
+    gChannel = rmq.channel;
+  } catch (err) {
+    console.log(err.message);
+    if (gConnection) {
+      closeConnection(gConnection, gChannel);
+      console.log(`Closing connections`);
+    }
+    throw err; // kill the app
+  }
+})().catch((err) => {
+  console.log(
+    `Shutting down node server listening on port ${APP_PRODUCER_PORT}`
+  );
+  server.close(); // Close the http server created with app.listen
+  process.exit(1); // A non-zero exit will cause the container to stop - depending on restart policy, it docker may try to restart it
+}); // () means call it now
